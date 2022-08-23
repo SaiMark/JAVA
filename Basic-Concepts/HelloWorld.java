f981137938df23a7bb5ccb2a01414b943a6609aa
SaiMark <59070894+SaiMark@users.noreply.github.com>
2022-08-23 19:30:17 +0930

    update readme
    comments, keywords

---
+++ Basic-Concepts/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	//main function, indicating the main program entrance
+	public static void main(String[] agrs) {
+		/*Output Statements
+		You can print the content inside the parentheses*/
+		System.out.println("HelloWorld");
+	}
+}
